87c01c51c1a7e8531d9e839d8b0c8e7914e7122f
Harkishen-Singh <harkishensingh@hotmail.com>
2019-05-12 22:25:34 +0530

    works as basic

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 console.warn('running scripts');
 var data = [
-    { "x_axis": 30, "y_axis": 30, "radius": 20, "color" : "green" },
-    { "x_axis": 70, "y_axis": 70, "radius": 20, "color" : "purple"},
-    { "x_axis": 110, "y_axis": 100, "radius": 20, "color" : "red"}];
+    { "x_axis": 300, "y_axis": 30, "radius": 20, "color" : "green" },
+    { "x_axis": 600, "y_axis": 70, "radius": 20, "color" : "purple"},
+    { "x_axis": 900, "y_axis": 100, "radius": 20, "color" : "red"},
+    { "x_axis": 1200, "y_axis": 30, "radius": 10, "color" : "blue" },
+    { "x_axis": 1500, "y_axis": 7, "radius": 20, "color" : "black"},
+    { "x_axis": 1800, "y_axis": 200, "radius": 30, "color" : "grey"}];
+
+var x_lines = [], y_lines = [];
+for(let x =0; x<data.length; x++) {
+    x_lines.push(data[x].x_axis);
+}
+console.log(x_lines);
 
 let container = d3.select('body').append('svg')
-                                    .attr('width', 200)
-                                    .attr('height', 200);
+                                    .attr('width', 2000)
+                                    .attr('height', 500);
 
 let circles = container.selectAll('circle')
     .data(data)
@@ -16,10 +25,18 @@ let circles = container.selectAll('circle')
 circles.attr('cx', function (d) {
     return d.x_axis;
 }).attr('cy', function (d) {
-    return d.y_axis;
+    return 200 - d.y_axis;
 }).attr('r', function (d) {
     return d.radius;
 }).attr('fill', function (d) {
     return d.color;
 });
+
+var line = d3.line().curve(d3.curveCardinal).x(function (a,b) {
+    return a.x_axis;
+}).y(function (a,b) {
+    return 200 - a.y_axis;
+});
+
+container.append('path').attr('d', line(data)).attr('stroke', 'black').attr('stroke-width', '2').attr("fill", "none");
 
